f8bbb8d993d2541666476efe7f8ed581ead8e300
Neonhash <darkdrive.gt@proton.me>
2025-05-03 18:10:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  SillyTavern plugin: use local NLLB translator
 // @author       Anton
-// @match        http://127.0.0.1:8000/*
+// @match        *://localhost:8000/*
+// @match        *://localhost:5007/*
 // @grant        none
 // ==/UserScript==
 
